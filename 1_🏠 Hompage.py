852f6350d1de63e80426f4ec8ff46d1e52bf0f1c
aghatem <69748322+aghatem@users.noreply.github.com>
2025-07-07 00:19:49 +0100

    Update 1_🏠 Hompage.py
    changed lines 3 & 5 to change background color

--- 1_🏠 Hompage.py
+++ 1_🏠 Hompage.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-st.set_page_config(page_title='Portfolio Homepage', page_icon='🏠',)
+st.set_page_config(page_title='Portfolio Homepage', layout="wide", initial_sidebar_state="expanded",page_icon='🏠',)
 
-
+st.markdown("""<style>.main {background-color: #f4f4f4;}
+</style>""", unsafe_allow_html=True)
 st.sidebar.success('Select a page aboe.')
 
 from PIL import Image
 
-st.write('# Welcome to My Data Analytics Portfolio!')
+st.write('# Welcome to My Projects Portfolio!')
 st.write('In this page, I showcase my data analytics projects where I leverage the power of data to gain insights, make informed decisions, and solve real-world problems.')
 
 # Add a title and image to the page
